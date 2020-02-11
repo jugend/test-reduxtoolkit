5466d858cc6633a7e116493bcac21948e7bc0336
Herryanto Siatono <hsiatono@paypal.com>
2020-02-11 15:51:00 +0800

    Add reactjs/toolkit

--- basic-counter/src/index.js
+++ basic-counter/src/index.js
@@ -1,20 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import Counter from './components/Counter'
-import counter from './reducers'
 
-const store = createStore(counter)
+// import { createStore } from 'redux'
+// import counter from './reducers'
+// const store = createStore(counter)
+
+// With Redux Toolkit
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: 0,
+    reducers: {
+        increment: state => state + 1,
+        decrement: state => state - 1,
+    },
+})
+
+const store = configureStore({
+    reducer: counterSlice.reducer,
+})
+
 const rootEl = document.getElementById('root')
 
-const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
-)
+// const render = () => ReactDOM.render(
+//   <Counter
+//     value={store.getState()}
+//     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+//     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+//   />,
+//   rootEl
+// )
+
+const { increment, decrement } = counterSlice.actions
+
+const render = () =>
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => store.dispatch(increment())}
+            onDecrement={() => store.dispatch(decrement())}
+        />,
+        rootEl,
+    )
 
 render()
 store.subscribe(render)
