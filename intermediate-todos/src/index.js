1fa2d399d3daf11612e2b34c555707d432958883
Herryanto Siatono <hsiatono@paypal.com>
2020-03-04 13:39:00 +0800

    add redux toolkit in todos

--- intermediate-todos/src/index.js
+++ intermediate-todos/src/index.js
@@ -4,8 +4,12 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
+import { configureStore } from '@reduxjs/toolkit'
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
+const  store = configureStore({
+  reducer: rootReducer
+})
 
 render(
   <Provider store={store}>
